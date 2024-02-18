a01506aba671fbad4b3dc3a821943d262afc3d7a
Artur-Lambo <lamboartur94@gmail.com>
2024-02-18 15:11:10 +0100

    SUppression et ajout de context pour les tests

--- Controllers/UserManagementController.cs
+++ Controllers/UserManagementController.cs
@@ -10,165 +10,147 @@ namespace Tasks_WEB_API.Controllers;
 
 public class UserManagementController : ControllerBase
 {
-    private readonly DailyTasksMigrationsContext _content;
+	private readonly DailyTasksMigrationsContext _content;
 
-    //private readonly ILogger<UserManagementController> _logger;
-    public UserManagementController( DailyTasksMigrationsContext context)
-    {
-        // _logger = logger; ILogger<UserManagementController> logger,
-        _content = context;
+	public UserManagementController(DailyTasksMigrationsContext context)
+	{
 
-    }
+		_content = context;
 
-    /// <summary>
-    /// Affiche la liste de tous les utilisateurs
-    /// </summary>
-    [HttpGet("~/GetUsers")]
-    public async Task<IActionResult> Get()
-    {
-       
-        try
-        {
-            var UsersContextDB = await _content.Utilisateurs.ToListAsync();
-            if (UsersContextDB.Any())
-            {
-                return Ok(UsersContextDB);
-            }
-            else
-            {
-                return NotFound("Pas de donnée utilisateur présente dans le contexte de base de données.");
-            }
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Erreur : lors de la collecte de donnée dans le contexte de base de donnée.");
-        }
-    }
+	}
 
-    /// <summary>
-    /// Affiche les informations sur un utilisateur
-    /// </summary>
-    /// <param name="ID"></param>
-    /// <returns></returns>
-    [HttpGet("~/SelectUser/{ID:int}")]
-    public async Task<IActionResult> SelectUser(int ID)
-    {
-         //_logger.LogInformation("Requête GET reçue avec l'ID : {Id}", ID);
-        try
-        {
-            var utilisateur = await _content.Utilisateurs.FindAsync(ID);
-            if (utilisateur != null)
-            {
-                return Ok(utilisateur);
-            }
-            return NotFound("Utilisateur non trouvé.");
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "Une erreur s'est produite ");
-        }
-    }
+	/// <summary>
+	/// Affiche la liste de tous les utilisateurs
+	/// </summary>
+	[HttpGet("~/GetUsers")]
+	public async Task<ActionResult<IEnumerable<Utilisateur>>> Get()
+	{
+		return await _content.Utilisateurs.ToListAsync();
+	}
 
-    /// <summary>
-    /// Créée un utilisateur
-    /// </summary>
-    /// <param name="identifiant"></param>
-    /// <param name="nom"></param>
-    /// <returns></returns>
-    [HttpPost("~/CreateUser/{identifiant:int}/{nom}")]
-    public async Task<IActionResult> CreateUser(int identifiant, string nom, string mdp, string role)
-    {
-        try
-        {
-            var DataBaseContext = await _content.Utilisateurs.ToListAsync();
+	/// <summary>
+	/// Affiche les informations sur un utilisateur
+	/// </summary>
+	/// <param name="ID"></param>
+	/// <returns></returns>
+	[HttpGet("~/SelectUser/{ID:int}")]
+	public async Task<ActionResult<IEnumerable<Utilisateur>>> SelectUser(int ID)
+	{
+	
+		try
+		{
+			var utilisateur = await _content.Utilisateurs.FindAsync(ID);
+			if (utilisateur != null)
+			{
+				return Ok(utilisateur);
+			}
+			return NotFound("Utilisateur non trouvé.");
+		}
+		catch (Exception)
+		{
+			return StatusCode(StatusCodes.Status500InternalServerError, "Une erreur s'est produite ");
+		}
+	}
 
-            if (DataBaseContext.Any(u => u.ID == identifiant))
-            {
-                return Conflict("Cet utilisateur est déjà présent.");
+	/// <summary>
+	/// Créée un utilisateur
+	/// </summary>
+	/// <param name="identifiant"></param>
+	/// <param name="nom"></param>
+	/// <returns></returns>
+	[HttpPost("~/CreateUser/{identifiant:int}/{nom}")]
+	public async Task<IActionResult> CreateUser(int identifiant, string nom, string mdp, string role)
+	{
+		try
+		{
+			var DataBaseContext = await _content.Utilisateurs.ToListAsync();
 
-            }
-            Utilisateur.Privilege privilege;
-            if (!Enum.TryParse(role, true, out privilege))
-            {
-                return BadRequest("Le rôle spécifié n'est pas valide.");
-            }
-            Utilisateur utilisateur = new Utilisateur() { ID = identifiant, Nom = nom, Pass = mdp, Role = privilege };
+			if (DataBaseContext.Any(u => u.ID == identifiant))
+			{
+				return Conflict("Cet utilisateur est déjà présent.");
 
-            // Enregistrement du nouvel utilisateur dans le contexte de base de données.
-            await _content.Utilisateurs.AddAsync(utilisateur);
-            await _content.SaveChangesAsync();
-            // Obtention de la liste mise à jour des utilisateurs après la création
-            var UsersContextDB = await _content.Utilisateurs.ToListAsync();
+			}
+			Utilisateur.Privilege privilege;
+			if (!Enum.TryParse(role, true, out privilege))
+			{
+				return BadRequest("Le rôle spécifié n'est pas valide.");
+			}
+			Utilisateur utilisateur = new Utilisateur() { ID = identifiant, Nom = nom, Pass = mdp, Role = privilege };
 
-            return Ok("La ressource a bien été créée");
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred");
-        }
-    }
+			// Enregistrement du nouvel utilisateur dans le contexte de base de données.
+			await _content.Utilisateurs.AddAsync(utilisateur);
+			await _content.SaveChangesAsync();
+			// Obtention de la liste mise à jour des utilisateurs après la création
+			var UsersContextDB = await _content.Utilisateurs.ToListAsync();
 
-    /// <summary>
-    /// Supprime un utilisateur
-    /// </summary>
-    /// <param name="ID"></param>
-    /// <returns></returns>
-    [HttpDelete("~/DeleteUser/{ID:int}")]
-    public async Task<IActionResult> DeleteUser(int ID)
-    {
-        var utilisateur = await _content.Utilisateurs.FindAsync(ID);
-        try
-        {
-            if (utilisateur == null)
-            {
-                return NotFound($"L'utilisateur [{ID}] n'existe plus dans le contexte de base de données");
-            }
-            _content.Utilisateurs.Remove(utilisateur);
-            await _content.SaveChangesAsync();
+			return Ok("La ressource a bien été créée");
+		}
+		catch (Exception)
+		{
+			return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred");
+		}
+	}
 
-            var users = _content.Utilisateurs.ToListAsync();
-            users.Result.Any();
+	/// <summary>
+	/// Supprime un utilisateur
+	/// </summary>
+	/// <param name="ID"></param>
+	/// <returns></returns>
+	[HttpDelete("~/DeleteUser/{ID:int}")]
+	public async Task<IActionResult> DeleteUser(int ID)
+	{
+		var utilisateur = await _content.Utilisateurs.FindAsync(ID);
+		try
+		{
+			if (utilisateur == null)
+			{
+				return NotFound($"L'utilisateur [{ID}] n'existe plus dans le contexte de base de données");
+			}
+			_content.Utilisateurs.Remove(utilisateur);
+			await _content.SaveChangesAsync();
 
-            return Ok("La donnée a bien été supprimée");
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                      "Error deleting data");
-        }
+			var users = _content.Utilisateurs.ToListAsync();
+			users.Result.Any();
 
-    }
+			return Ok("La donnée a bien été supprimée");
+		}
+		catch (Exception)
+		{
+			return StatusCode(StatusCodes.Status500InternalServerError,
+					  "Error deleting data");
+		}
 
-    /// <summary>
-    /// Met à jour les informations d'un utilisateur
-    /// </summary>
-    /// <returns></returns>
-    [HttpPut("~/UpdateUser")]
-    public async Task<IActionResult> UpdateUser([FromBody] Utilisateur utilisateur)
-    {
-        try
-        {
-            var item = await _content.Utilisateurs.FindAsync(utilisateur.ID);
+	}
 
-            if (item is null)
-            {
-                return NotFound($"Cet utilisateur n'existe plus dans le contexte de base de données");
-            }
-            if (item.ID == utilisateur.ID)
-            {
-                item.Nom = utilisateur.Nom;
-                item.Pass=utilisateur.Pass;
-                item.Role=utilisateur.Role;
-                await _content.SaveChangesAsync();
+	/// <summary>
+	/// Met à jour les informations d'un utilisateur
+	/// </summary>
+	/// <returns></returns>
+	[HttpPut("~/UpdateUser")]
+	public async Task<IActionResult> UpdateUser([FromBody] Utilisateur utilisateur)
+	{
+		try
+		{
+			var item = await _content.Utilisateurs.FindAsync(utilisateur.ID);
 
-            }
-            return Ok($"Les infos de l'utilisateur [{item.ID}] ont bien été modifiées.");
-        }
-        catch (Exception)
-        {
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                      "Error deleting data");
-        }
-    }
+			if (item is null)
+			{
+				return NotFound($"Cet utilisateur n'existe plus dans le contexte de base de données");
+			}
+			if (item.ID == utilisateur.ID)
+			{
+				item.Nom = utilisateur.Nom;
+				item.Pass = utilisateur.Pass;
+				item.Role = utilisateur.Role;
+				await _content.SaveChangesAsync();
+
+			}
+			return Ok($"Les infos de l'utilisateur [{item.ID}] ont bien été modifiées.");
+		}
+		catch (Exception)
+		{
+			return StatusCode(StatusCodes.Status500InternalServerError,
+					  "Error deleting data");
+		}
+	}
 }
--- Tasks_WEB_API.Tests/ControllerTest.cs
+++ Tasks_WEB_API.Tests/ControllerTest.cs
@@ -1,19 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Xunit;
 using Tasks_WEB_API.Controllers;
 using Tasks_WEB_API.Models;
-using Xunit;
-namespace Tasks_WEB_API.Tests;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 
-public class ControllerTest
+namespace Tasks_WEB_API.Tests
 {
-	private readonly DailyTasksMigrationsContext _content;
-	[Fact]
-	//[Theory]
-	public async Task Get_AllUsers( )
+	public class ControllerTest
 	{
-		  // Arrange
-		  UserManagementController context = new UserManagementController(_content);
-		  // Act
-		  await context.Get();
-		  // Assert
+		// [Fact]
+		// public async Task Get_ReturnsNotFound_ForEmptyUsersData()
+		// {
+
+		// 	// Arrange
+		// 	var contextMock = new Mock<DailyTasksMigrationsContext>();
+		// 	contextMock.Setup<DbSet<Utilisateur>>(x => x.Utilisateurs).Returns(TestDataHelper.GetFakeEmployeeList());
+
+		// 	//Act
+		// 	UserManagementController controller = new(contextMock.Object);
+		// 	var utilisateur = await controller.Get();
+
+		// 	//Assert
+		// 	Assert.NotNull(utilisateur);
+		// }
 	}
 }
+
